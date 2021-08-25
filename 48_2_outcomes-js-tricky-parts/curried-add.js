4af18f2afa97221387efe08ebc89149fe24723fe
Joyce-w <hellojoycewong@gmail.com>
2021-08-25 13:08:15 -0700

    test/add curried-add

--- 48_2_outcomes-js-tricky-parts/curried-add.js
+++ 48_2_outcomes-js-tricky-parts/curried-add.js
@@ -1,5 +1,13 @@
 function curriedAdd(total) {
-
+    //return 0 if nothing is passed into curriedAdd
+    if (total === undefined) return 0;
+    return function addNum(num) {
+        //return total if empty paramater
+        if (num === undefined) return total;
+        //if there is a number, add it to the total and return a new addNum
+        total += num;
+        return addNum;
+    }
 }
 
 module.exports = { curriedAdd };
